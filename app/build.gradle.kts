0538405c65e16284e073daa4cf159b1919a55a67
alina tarasova <alishka.tarasova@gmail.com>
2024-02-19 16:08:06 +0300

    Updated build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,16 +22,22 @@ application {
 }
 
 dependencies {
-    implementation("org.apache.commons:commons-lang3:3.14.0")
-    implementation("org.apache.commons:commons-collections4:4.4")
+    implementation("org.apache.commons:commons-text:1.10.0")
+    //implementation("org.apache.commons:commons-collections4:4.4")
     testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core:3.25.1")
 
-    implementation("io.javalin:javalin:5.6.1")
-    implementation("org.slf4j:slf4j-simple:2.0.7")
-    implementation("io.javalin:javalin-rendering:5.6.0")
-    implementation("gg.jte:jte:3.0.1")
+    implementation("io.javalin:javalin:5.6.2")
+    implementation("io.javalin:javalin-bundle:5.6.2")
+    implementation("io.javalin:javalin-rendering:5.6.2")
+    implementation("gg.jte:jte:3.1.0")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
+
+    implementation("com.h2database:h2:2.2.222")
+    implementation("com.zaxxer:HikariCP:5.0.1")
+
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
 
 }
 
