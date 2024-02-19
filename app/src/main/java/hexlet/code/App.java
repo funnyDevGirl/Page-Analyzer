5e52227ceab8e848b2f98844af4c8d5e9928dd64
alina tarasova <alishka.tarasova@gmail.com>
2024-02-19 16:16:01 +0300

    Updated App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,34 +1,59 @@
 package hexlet.code;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import hexlet.code.repository.BaseRepository;
 import io.javalin.Javalin;
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.sql.SQLException;
+import java.util.stream.Collectors;
+
+@Slf4j
 public class App {
 
     private static int getPort() {
         String port = System.getenv().getOrDefault("PORT", "7070");
-        return Integer.valueOf(port);
+        return Integer.parseInt(port);
     }
 
-//    private static String readResourceFile(String fileName) throws IOException {
-//        var inputStream = HelloWorld.class.getClassLoader().getResourceAsStream(fileName);
-//        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
-//            return reader.lines().collect(Collectors.joining("\n"));
-//        }
-//    }
+    private static String readResourceFile() throws IOException {
+        var inputStream = App.class.getClassLoader().getResourceAsStream("schema.sql");
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        }
 
-    public static Javalin getApp() {
-        var app = Javalin.create(config -> {
-            config.plugins.enableDevLogging();
-        });
+    }
 
-        app.get("/", ctx -> {
-            ctx.result("Hello World");
-        });
+    public static Javalin getApp() throws IOException, SQLException {
+
+        var hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(System.getenv()
+                .getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;"));
+
+        var dataSource = new HikariDataSource(hikariConfig);
+
+        String sql = readResourceFile();
+
+        //log.info(sql);
+        try (var connection = dataSource.getConnection();
+             var statement = connection.createStatement()) {
+            statement.execute(sql);
+        }
+        BaseRepository.dataSource = dataSource;
+
+        var app = Javalin.create(config -> config.plugins.enableDevLogging());
+
+        app.get("/", ctx -> ctx.result("Hello World"));
 
         return app;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, SQLException {
         Javalin app = getApp();
         app.start(getPort());
     }
