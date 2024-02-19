9b3336a123230507d6284b18ada38aadcde8e255
alina tarasova <alishka.tarasova@gmail.com>
2024-02-19 09:21:07 +0300

    Added App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,35 @@
 package hexlet.code;
 
-public class Main {
+import io.javalin.Javalin;
+
+public class App {
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.valueOf(port);
+    }
+
+//    private static String readResourceFile(String fileName) throws IOException {
+//        var inputStream = HelloWorld.class.getClassLoader().getResourceAsStream(fileName);
+//        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+//            return reader.lines().collect(Collectors.joining("\n"));
+//        }
+//    }
+
+    public static Javalin getApp() {
+        var app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+
+        app.get("/", ctx -> {
+            ctx.result("Hello World");
+        });
+
+        return app;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Javalin app = getApp();
+        app.start(getPort());
     }
 }
