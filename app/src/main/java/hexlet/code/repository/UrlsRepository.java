c3eadbfad0d7829edfe91f60f910a8c0ac5eb744
alina tarasova <alishka.tarasova@gmail.com>
2024-02-20 20:27:23 +0300

    Added UrlsRepository.java

---
+++ app/src/main/java/hexlet/code/repository/UrlsRepository.java
@@ -0,0 +1,80 @@
+package hexlet.code.repository;
+
+import hexlet.code.model.Url;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class UrlsRepository extends BaseRepository {
+    public static void save(Url url) throws SQLException {
+        String sql = "INSERT INTO urls (name, createdAt) VALUES (?, ?)";
+        try (var conn = dataSource.getConnection();
+             var preparedStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setString(1, url.getName());
+            preparedStatement.setTimestamp(2, url.getCreatedAt());
+            preparedStatement.executeUpdate();
+
+            var generatedKey = preparedStatement.getGeneratedKeys();
+
+            if (generatedKey.next()) {
+                url.setId(generatedKey.getLong(1));
+            } else {
+                throw new SQLException("DB have not returned an id after saving an entity");
+            }
+        }
+    }
+
+    public static Optional<Url> find(Long id) throws SQLException {
+        String sql = "SELECT * FROM urls WHERE id = ?";
+        try (var conn = dataSource.getConnection();
+             var stmt = conn.prepareStatement(sql)) {
+            stmt.setLong(1, id);
+            var resultSet = stmt.executeQuery();
+            if (resultSet.next()) {
+                var name = resultSet.getString("name");
+                var createdAt = resultSet.getTimestamp("createdAt");
+                var url = new Url(name, createdAt);
+                url.setId(id);
+                return Optional.of(url);
+            }
+            return Optional.empty();
+        }
+    }
+
+    public static List<Url> getEntities() throws SQLException {
+        String sql = "SELECT * FROM urls";
+        try (var conn = dataSource.getConnection();
+             var stmt = conn.prepareStatement(sql)) {
+            var resultSet = stmt.executeQuery();
+            var result = new ArrayList<Url>();
+            while (resultSet.next()) {
+                var id = resultSet.getLong("id");
+                var name = resultSet.getString("name");
+                var createdAt = resultSet.getTimestamp("createdAt");
+                var url = new Url(name, createdAt);
+                url.setId(id);
+                result.add(url);
+            }
+            return result;
+        }
+    }
+
+    public static boolean existsByName(String name) throws SQLException {
+        return getEntities().stream()
+                .anyMatch(value -> value.getName().equals(name));
+    }
+
+    public static void delete(int id) {
+        String sql = "DELETE FROM urls WHERE id = ?";
+        try (var conn = dataSource.getConnection();
+             var stmt = conn.prepareStatement(sql)) {
+            stmt.setLong(1, id);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
