2c91c0929789b8efb590e43210b2188e65f4f9f4
alina tarasova <alishka.tarasova@gmail.com>
2024-02-21 11:09:53 +0300

    Added AppTest.java

---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,63 @@
+package hexlet.code;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Date;
+
+import hexlet.code.repository.UrlsRepository;
+import hexlet.code.model.Url;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import io.javalin.Javalin;
+import io.javalin.testtools.JavalinTest;
+
+
+public class AppTest {
+    Javalin app;
+
+    @BeforeEach
+    public final void setUp() throws IOException, SQLException {
+        app = App.getApp();
+    }
+
+    @Test
+    public void testMainPage() {
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get("/");
+            assertThat(response.code()).isEqualTo(200);
+            assertThat(response.body().string()).contains("Анализатор страниц");
+        });
+    }
+
+    @Test
+    public void testUrlsPage() {
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get("/urls");
+            assertThat(response.code()).isEqualTo(200);
+        });
+    }
+
+    @Test
+    public void testUrlPage() throws SQLException {
+        var url = new Url("https://www.mail.ru", new Timestamp(new Date().getTime()));
+        UrlsRepository.save(url);
+
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get("/urls/" + url.getId());
+            assertThat(response.code()).isEqualTo(200);
+            assertThat(response.body().string()).contains("https://www.mail.ru");
+        });
+    }
+
+    @Test
+    void testUrlNotFound() {
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get("/urls/999999");
+            assertThat(response.code()).isEqualTo(404);
+        });
+    }
+}
