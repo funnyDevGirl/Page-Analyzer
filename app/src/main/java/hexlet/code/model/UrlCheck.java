c5800c1d9269b1fcc11689378ac01f50fdb24583
alina tarasova <alishka.tarasova@gmail.com>
2024-02-27 21:16:03 +0300

    Updated UrlCheck.java

--- app/src/main/java/hexlet/code/model/UrlCheck.java
+++ app/src/main/java/hexlet/code/model/UrlCheck.java
@@ -7,19 +7,29 @@ import lombok.Setter;
 import java.sql.Timestamp;
 
 @Getter
-@Setter
 @AllArgsConstructor
 public class UrlCheck {
+    @Setter
     private int id;
+
     private int urlId;
     private int statusCode;
     private String title;
     private String h1;
     private String description;
+    @Setter
     private Timestamp createdAt;
 
-    public UrlCheck(int id, int statusCode, String title, String h1, String description, Timestamp createdAt) {
-        this.id = id;
+    public UrlCheck(int urlId, int statusCode, String title, String h1, String description) {
+        this.id = urlId;
+        this.statusCode = statusCode;
+        this.title = title;
+        this.h1 = h1;
+        this.description = description;
+    }
+
+    public UrlCheck(int urlId, int statusCode, String title, String h1, String description, Timestamp createdAt) {
+        this.urlId = urlId;
         this.statusCode = statusCode;
         this.title = title;
         this.h1 = h1;
