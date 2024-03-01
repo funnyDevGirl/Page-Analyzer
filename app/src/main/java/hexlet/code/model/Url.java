b8e1131b24e6da241257cbb8c8417dae29b154d5
alina tarasova <alishka.tarasova@gmail.com>
2024-03-01 06:42:58 +0300

    Updated Url.java, UrlCheck.java

--- app/src/main/java/hexlet/code/model/Url.java
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -3,19 +3,27 @@ package hexlet.code.model;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import java.sql.Timestamp;
 
 @Getter
 @Setter
 @ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public final class Url {
-    private Long id;
+    private int id;
 
     @ToString.Include
     private String name;
 
     private Timestamp createdAt;
+
+    public Url(String name) {
+        this.name = name;
+    }
 
     public Url(String name, Timestamp createdAt) {
         this.name = name;
--- app/src/main/java/hexlet/code/model/UrlCheck.java
+++ app/src/main/java/hexlet/code/model/UrlCheck.java
@@ -7,21 +7,28 @@ import lombok.Setter;
 import java.sql.Timestamp;
 
 @Getter
+@Setter
 @AllArgsConstructor
 public class UrlCheck {
-    @Setter
     private int id;
-
     private int urlId;
     private int statusCode;
     private String title;
     private String h1;
     private String description;
-    @Setter
     private Timestamp createdAt;
 
+    public UrlCheck(int id, int urlId, int statusCode, String title, String h1, String description) {
+        this.id = id;
+        this.urlId = urlId;
+        this.statusCode = statusCode;
+        this.title = title;
+        this.h1 = h1;
+        this.description = description;
+    }
+
     public UrlCheck(int urlId, int statusCode, String title, String h1, String description) {
-        this.id = urlId;
+        this.urlId = urlId;
         this.statusCode = statusCode;
         this.title = title;
         this.h1 = h1;
