7bce4f8ad01fde7f9db5b2c2de456ace929054b2
alina tarasova <alishka.tarasova@gmail.com>
2024-03-03 17:24:16 +0300

    Updated schema.sql

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -4,7 +4,8 @@ DROP TABLE IF EXISTS urls;
 CREATE TABLE urls (
     id BIGINT GENERATED ALWAYS AS IDENTITY UNIQUE NOT NULL,
     name VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP NOT NULL
+    created_at TIMESTAMP NOT NULL,
+    CONSTRAINT pk_url PRIMARY KEY (id)
 );
 
 CREATE TABLE url_checks (
@@ -15,5 +16,13 @@ CREATE TABLE url_checks (
     title VARCHAR(255),
     description TEXT,
     created_at TIMESTAMP,
-    FOREIGN KEY (url_id) REFERENCES urls (id)
+    FOREIGN KEY (url_id) REFERENCES urls (id),
+    CONSTRAINT pk_url_checks PRIMARY KEY (id)
 );
+
+CREATE INDEX ix_url_check_url_id ON url_checks (url_id);
+ALTER TABLE url_checks
+    ADD CONSTRAINT fk_url_checks_url_id
+    FOREIGN KEY (url_id)
+    REFERENCES urls (id)
+    ON DELETE RESTRICT ON UPDATE RESTRICT;
