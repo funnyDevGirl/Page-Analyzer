2dc127b2cbd243a989fa0ee5c18f607f1ca5c167
alina tarasova <alishka.tarasova@gmail.com>
2024-03-03 13:42:14 +0300

    Updated UrlPage.java

--- app/src/main/java/hexlet/code/dto/urls/UrlPage.java
+++ app/src/main/java/hexlet/code/dto/urls/UrlPage.java
@@ -7,11 +7,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @AllArgsConstructor
 @Getter
+@Setter
 public class UrlPage extends BasePage {
     private Url url;
     private List<UrlCheck> urlChecks;
