c21ef6ce46f25c6b464eb4e63cdf86d6bd8502fd
alina tarasova <alishka.tarasova@gmail.com>
2024-03-03 12:58:46 +0300

    Updated schema.sql

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE urls (
 CREATE TABLE url_checks (
     id BIGINT GENERATED ALWAYS AS IDENTITY UNIQUE NOT NULL,
     url_id BIGINT REFERENCES urls (id) NOT NULL,
-    status_code BIGINT,
+    status_code INTEGER,
     h1 VARCHAR(255),
     title VARCHAR(255),
     description TEXT,
